b66c01fa11ce9fbf17e1ab24548ae57ed155d33c
Evan BOISSONNOT <evan@boissonnot.fr>
2024-05-13 17:24:57 +0200

    feat: day 01

---
+++ SdA.Games.RPG/SdA.Games.RPG.UI.Console/ChoixMenu.cs
@@ -0,0 +1,12 @@
+﻿namespace SdA.Games.RPG.UI.Console
+{
+    public enum ChoixMenu
+    {
+        SaisieProfil = 1,
+        NouvellePartie,
+        PartieExistante,
+        ChargerPartie,
+        Options,
+        Sortie = -100
+    }
+}
--- SdA.Games.RPG/SdA.Games.RPG.UI.Console/Program.cs
+++ SdA.Games.RPG/SdA.Games.RPG.UI.Console/Program.cs
@@ -1,25 +1,147 @@
 ﻿// je suis dans la méthode static void Main
 
 // récupère le premier argument passé à ma commande exe
-// string argUn = args[0];
+using SdA.Games.RPG.UI.Console;
+using System.Globalization;
+
+var culture = new CultureInfo("en-EN");
+System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+
+string argUn = args[0];
+char premierChar = argUn[0]; // tableau de caractère
 
 #region Infos formation
 // char unCharactere = 'a';
 // Console.WriteLine(title.Length);
 
+//DateTime date = DateTime.Now;
+//Console.WriteLine(date.ToString("dddd dd MMMM yyyy", new CultureInfo("fr-CA")));
 #endregion
 
 #region Exécution du programme
 string title = "Seigneur des anneaux Factory";
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine(GetTitreFormate(title));
+// {
+var nomJoueur = DemanderNomJoueur();
 
-title = "Une aventure de Sam";
+// }
+
+// Concaténer : v1
+// title = "Une aventure de " + nomJoueur;
+// Concaténer : v2 => interpolation
+// title = $"Une aventure de {nomJoueur.ToUpper()} // {nomJoueur.ToLower()}";
+title = string.Format("Une aventure de {0} / {0}", nomJoueur.ToUpper());
+
 Console.WriteLine(GetTitreFormate(title));
 Console.ForegroundColor = ConsoleColor.White;
+
+
+AfficherMenu();
+SaisirProfil();
 #endregion
 
 #region Coeur d'application
+void SaisirProfil()
+{
+    VerifierAgeJoueur();
+}
+
+DateTime DemanderDateNaissance()
+{
+    //DateTime dateTime = DateTime.Parse(dateSaisie);
+    DateTime dateReel = DateTime.MinValue;
+    bool isConversionOK = false;
+
+    do
+    {
+        Console.WriteLine("Saisis la date de naissance");
+        string dateSaisie = Console.ReadLine();
+
+        isConversionOK = DateTime.TryParse(dateSaisie, out dateReel);
+
+    } while (!isConversionOK);
+
+    Console.WriteLine("Date récupérée {0}", dateReel.ToString());
+
+    return dateReel;
+}
+
+void VerifierAgeJoueur()
+{
+    const int AgeRequis = 13;
+    // DateOnly
+    // TimeOnly
+    DateTime dateNaissance = DemanderDateNaissance();
+    TimeSpan soustraction = DateTime.Now - dateNaissance;
+
+    int age = (int)soustraction.TotalDays / 365;
+
+    if (age < AgeRequis)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Tu n'as pas l'âge requis, petit coquin ;)");
+        Environment.Exit(0);
+    }
+}
+
+void AfficherMenu()
+{
+    //string[] menuItems = new string[6];
+    //menuItems[0] = "0. Saisie profil";
+
+    ChoixMenu choixMenu = ChoixMenu.NouvellePartie;
+
+    string[] menuItems = new string[]
+    {
+        "Saisie profil",
+        "Démarrer la partie",
+        "Reprendre la partie en cours",
+        "Charger une partie existante",
+        "Options",
+        "Quitter"
+    };
+
+    Console.WriteLine("MENU");
+    for (int i = 0; i < menuItems.Length; i++)
+    {
+        AfficherLigneMenu(i, menuItems[i]);
+    }
+}
+
+void ChoixItem()
+{
+    ChoixMenu choix = ChoixMenu.Options;
+
+    switch (choix)
+    {
+        case ChoixMenu.SaisieProfil:
+            {
+
+            }
+            break;
+
+        case ChoixMenu.Options:
+            {
+
+            }
+            break;
+    }
+}
+
+void AfficherLigneMenu(int i, string content)
+{
+    Console.WriteLine("{0}. {1}", i, content.ToUpper());
+}
+
+string DemanderNomJoueur()
+{
+    Console.WriteLine("Donne moi le nom du héros, {0}", "stp");
+    var nomJoueur = Console.ReadLine();
+
+    return nomJoueur;
+}
+
 // fonction locale
 string GetTitreFormate(string title)
 {
